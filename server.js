82c9a29ac810c0db4a4ad900fc9d2c49d4b0fc34
MathisGredtYnov <113118860+MathisGredtYnov@users.noreply.github.com>
2025-04-01 10:54:21 +0200

    change link

--- server.js
+++ server.js
@@ -2,15 +2,28 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Servir les fichiers statiques (mets tes fichiers dans 'public')
-app.use(express.static(path.join(__dirname, 'Enigme2')));
+// Servir les fichiers statiques
+app.use(express.static(path.join(__dirname, 'Enigme1/')));
+app.use(express.static(path.join(__dirname, 'Enigme2/')));
+app.use(express.static(path.join(__dirname, 'Enigme3/')));
+
+/* Servir les fichiers statiques
+app.use(express.static(path.join(__dirname, 'Enigme1/css')));
+app.use(express.static(path.join(__dirname, 'Enigme2/css')));
+app.use(express.static(path.join(__dirname, 'Enigme3/css')));
+
+// Servir les fichiers statiques
+app.use(express.static(path.join(__dirname, 'Enigme1/js')));
+app.use(express.static(path.join(__dirname, 'Enigme2/js')));
+app.use(express.static(path.join(__dirname, 'Enigme3/js')));
+ */
 
 // Route par défaut
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'Enigme1/html', 'index.html'));
 });
 
-// Démarrer le serveur sur le port 3000
+// Démarrer le serveur
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur http://localhost:${PORT}`);
